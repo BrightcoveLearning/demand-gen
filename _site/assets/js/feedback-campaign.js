ebcd870c4866bec85c0601307af5c9afe3b56265
Robert Crooks <rcrooks@brightcove.com>
2020-01-16 11:24:55 -0500

    rebuild for css consolidations

---
+++ _site/assets/js/feedback-campaign.js
@@ -0,0 +1,7 @@
+var fby = fby || [];
+fby.push(['showTab', {id: '14985', position: 'right', color: 'rgb(193, 66, 217)'}]);
+(function () {
+    var f = document.createElement('script'); f.type = 'text/javascript'; f.async = true;
+    f.src = '//cdn.feedbackify.com/f.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(f, s);
+})();
