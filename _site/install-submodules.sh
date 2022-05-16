c4a719c6ad0dac9e26442f8c6b9fb2ee2dc2d344
Matthew Boles <mboles@brightcove.com>
2022-05-16 13:46:45 -0400

    Initial submodule setup

---
+++ _site/install-submodules.sh
@@ -0,0 +1,8 @@
+git submodule add https://github.com/BrightcoveLearning/s-scripts
+cd _includes
+git submodule add https://github.com/BrightcoveLearning/s-media
+cd ..
+cd assets/images
+git submodule add https://github.com/BrightcoveLearning/s-media-img
+cd ..
+git submodule update --init --recursive
