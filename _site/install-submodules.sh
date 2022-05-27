a70411e4761c1d093189b4e4a4ec15f10e7406ed
Matthew Boles <mboles@brightcove.com>
2022-05-27 11:01:38 -0400

    Added campaign submodules

--- _site/install-submodules.sh
+++ _site/install-submodules.sh
@@ -3,10 +3,12 @@ cd _includes
 git submodule add https://github.com/BrightcoveLearning/s-jekyll-page-includes
 git submodule add https://github.com/BrightcoveLearning/s-media
 git submodule add https://github.com/BrightcoveLearning/s-support
+git submodule add https://github.com/BrightcoveLearning/s-campaign
 cd ..
 cd assets/images
 git submodule add https://github.com/BrightcoveLearning/s-media-img
 git submodule add https://github.com/BrightcoveLearning/s-support-img
 git submodule add https://github.com/BrightcoveLearning/s-feather-img
+git submodule add https://github.com/BrightcoveLearning/s-campaign-img
 cd ..
 git submodule update --init --recursive
