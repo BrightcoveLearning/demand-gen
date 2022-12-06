3dd00c73a0fc20979666325c2f90d054fd267afd
Robert <rcrooks@brightcove.com>
2022-12-06 13:18:46 -0500

    rebuild for new subm

--- install-submodules.sh
+++ install-submodules.sh
@@ -17,6 +17,7 @@ git submodule add https://github.com/BrightcoveLearning/s-campaign-img
 cd ..
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-favicons
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-stylesheets
+git submodule add https://github.com/BrightcoveLearning/s-site-assets-template-stylesheets
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-scripts
 git submodule add https://github.com/BrightcoveLearning/s-site-assets-images
 cd ..
